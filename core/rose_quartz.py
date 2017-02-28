063bdcdafc12fb1afcfb41cb6049936f494f6434
Tamby Kaghdo <tkaghdo@accordantmedia.com>
2017-02-28 16:44:31 -0500

    more of the same

--- core/rose_quartz.py
+++ core/rose_quartz.py
@@ -3,27 +3,64 @@ import json
 from user.user import User
 
 
-
-def is_user_exists(user, database="file"):
-    email = user.get_email()
-    user_exists = False
-    user_id = None
+def add_user(user, new_user_id, database="file"):
+    user_dict = {"id": new_user_id, "name": user.user_name, "email": user.email, "level": user.level}
     if database == "file":
         try:
-            with open('../data/users.json') as data_file:
+            with open('../data/data.json') as data_file:
                 data = json.load(data_file)
-                for key in data:
-                    user_list = data[key]
-                    for user_dict in user_list:
-                        if user_dict["email"] == email:
-                            user_exists = True
-                            user_id = user_dict["id"]
-                            break
+                data.append(user_dict)
+                print(data)
+
 
         except FileNotFoundError as e:
             print(e)
             sys.exit(e.errno)
+
+
+def get_max_user_id(database="file"):
+    if database == "file":
+        try:
+            with open('../data/data.json') as data_file:
+                data = json.load(data_file)
+                max_id = None
+                for user in data:
+                    if user["id"] > max_id:
+                        max_id = user["id"]
+
+        except FileNotFoundError as e:
+            print(e)
+            sys.exit(e.errno)
+
+    return max_id
+
+
+def is_user_exists(user, database="file"):
+    """
+    looks if a user exists or not in the database
+    :param user: a user object
+    :param database: the database where the users live. By default its a JSON file
+    :return: a boolean if the user exists or not and the id of the user if exists
+    """
+    email = user.email
+    user_exists = False
+    user_id = None
+    if database == "file":
+        try:
+            with open('../data/data.json') as data_file:
+                data = json.load(data_file)
+                for user in data:
+                    if user["email"] == email:
+                        user_exists = True
+                        user_id = user["id"]
+                        break
+
+        except FileNotFoundError as e:
+            print(e)
+            sys.exit(e.errno)
+
     return user_exists, user_id
+
 
 def main():
 
@@ -32,15 +69,28 @@ def main():
     # rq dict will come from the front end
     user_dict = {
         "name": "poop",
-        "email": "aa@gmail.com",
+        "email": "mm@gmail.com",
     }
 
     # create user object
     u2 = User(user_dict)
 
     # place this new user in the "database"
-    # --> check if the user is already in the database, provide a message if so
-    print(is_user_exists(u2))
+    # check if the user is already in the database, provide a message if so
+    user_status, user_id = is_user_exists(u2)
+
+    if user_status == True:
+        print("User {0} already exists with email {1}".format(u2.get_user_name(), u2.get_email()))
+        sys.exit(1)
+    else:
+        # add user to database
+        new_user_id = None
+        if get_max_user_id() == None:
+            new_user_id = 1
+        else:
+            new_user_id = get_max_user_id() + 1
+            add_user(u2,new_user_id)
+
 
     # --> if no user exists then create a record of the user
 
