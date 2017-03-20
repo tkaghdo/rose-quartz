e167e0731a68179aea672f148341576c5abc0e58
Tamby Kaghdo <tamby.kaghdo@gmail.com>
2017-03-19 20:17:37 -0400

    iphone signup

---
+++ frontend/iphone/RoseQuartz/RoseQuartz/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  RoseQuartz
+//
+//  Created by Tamby Kaghdo on 3/19/17.
+//  Copyright © 2017 Tamby Kaghdo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
