e8a6f32ae208011f05896e521fd04fa7c24d8ea8
Tamby Kaghdo <tamby.kaghdo@gmail.com>
2017-03-22 19:58:31 -0400

    created json

--- frontend/iphone/RoseQuartz/RoseQuartz/ViewController.swift
+++ frontend/iphone/RoseQuartz/RoseQuartz/ViewController.swift
@@ -17,21 +17,71 @@ class ViewController: UIViewController {
     @IBOutlet weak var pwTextField: UITextField!
     
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     //MARK: Actions
     
     @IBAction func signup(_ sender: UIButton) {
-        print("SIGN-UP PRESSED")
+        
+        //TODO: encode the values
+        let userDict: [String:String] = [
+            "name" : nameTextField.text!,
+            "email" : emailTextField.text!,
+            "password" : pwTextField.text!
+        ]
+        // create the user json
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: userDict, options: .prettyPrinted)
+            // here "jsonData" is the dictionary encoded in JSON data
+            
+            
+            // write the the json to file
+            let file = "qqqq.txt" //this is the file. we will write to and read from it
+            
+            print(jsonData)
+            let text = jsonData //just a tex
+            
+            if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                //writing
+                do {
+                    print(dir)
+                    let path = dir.appendingPathComponent(file)
+                    try text.write(to: path)
+                    print("***")
+                }
+                catch {print(error)}
+                defer{} //TODO: delete json file
+            }
+            
+        }
+        catch {
+            print(error)
+        }
+        
+        
+        /*
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            //writing
+            do {
+                print(dir)
+                let path = dir.appendingPathComponent(file)
+                try text.write(to: path, atomically: false, encoding: String.Encoding.utf8)
+                print("***")
+            }
+            catch {print(error)}
+            //TODO: delete json file
+            defer{}
+        }
+        */
     }
 }
 
