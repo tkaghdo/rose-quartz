589a0d4a4345846b8b4e4f46a2db716690a13383
Tamby Kaghdo <tamby.kaghdo@gmail.com>
2017-03-28 20:59:27 -0400

    sample post works

--- frontend/iphone/RoseQuartz/RoseQuartz/ViewController.swift
+++ frontend/iphone/RoseQuartz/RoseQuartz/ViewController.swift
@@ -80,6 +80,28 @@ class ViewController: UIViewController {
         task.resume()
     }
     
+    // post
+    func add_user(){
+        var request = URLRequest(url: URL(string: "http://localhost:8000/signup/tamby")!)
+        request.httpMethod = "POST"
+        let postString = "id=13&name=Jack"
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+    }
     
     
     //MARK: Actions
@@ -98,6 +120,8 @@ class ViewController: UIViewController {
         // get_request()
         
         // TODO: post user_dict to backend
+        
+        add_user()
     }
 }
 
