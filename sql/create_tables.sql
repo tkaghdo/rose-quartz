2c7c89985467cb454880fce0fca7890f48e70dc0
Tamby Kaghdo <tamby.kaghdo@gmail.com>
2017-03-31 23:24:16 -0400

    mockup and new tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,31 @@
 
+-- this is a legacy table
 create table users (
     id int primary key not null,
     email varchar(50) not null UNIQUE,
     name varchar(50),
     password varchar(50),
     level int default 0
+);
+
+
+create table dim_user(
+  user_id int primary key not null,
+  email varchar(50) not null UNIQUE,
+  name varchar(50) not null,
+  password varchar(50) not null,
+  date_created date,
+  date_updated date,
+  last_login date,
+  quests varchar(1000) -- this is a pipe delimeted field with the languages the user is enrolled in
+);
+
+create table dim_language(
+  language_id int primary key not null,
+  language varchar(50) not null
+);
+
+create table dim_question(
+  question_id int primary key not null,
+  question varchar(3000) not null
 );
